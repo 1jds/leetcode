181227d0659ce84b973be61501f821becfd95faf
James <jamesdstacey2@gmail.com>
2024-01-15 00:16:03 +1100

    combined both palindrome no 9 methods in one file

--- palindromeNumberNo9.js
+++ palindromeNumberNo9.js
@@ -1,3 +1,5 @@
+// THE NON-STRING METHOD
+
 /**
  * @param {number} x
  * @return {boolean}
@@ -40,3 +42,32 @@ console.log(isPalindrome(10));
 // Date: 15/01/2024
 // Runtime: 124ms - beats 92.96% of users with JavaScript
 // Memory: 50.73MB - beats 69.77% of users with JavaScript
+
+// THE STRING METHOD
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function (x) {
+  s = x + "";
+  arr = s.split("");
+  halfLength = Math.floor(s.length / 2);
+  // console.log(length);
+  for (i = 0; i < halfLength; i++) {
+    if (arr[i] != arr[arr.length - (i + 1)]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
+
+// This is the method of casting the int to a string and then comparing characters
+// Date: 14/01/2024
+// Runtime: 131ms - beats 83.50% of users with JavaScript
+// Memory: 50.99MB - beats 58.08% of users with JavaScript
