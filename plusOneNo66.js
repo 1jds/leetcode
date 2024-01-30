d02e0d880fdee762f8306db6934aef3537676acf
1jds <jamesdstacey2@gmail.com>
2024-01-31 01:13:46 +1100

    two solutions finished

--- plusOneNo66.js
+++ plusOneNo66.js
@@ -1,20 +1,45 @@
+// Version #1
+
 /**
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  console.log("the length of this array is...", digits.length);
   let stringified = digits.join("");
-  console.log("stringified: ", stringified);
   let joined = BigInt(stringified);
-  console.log("joined: ", joined);
   joined++;
-  console.log("joined: ", joined);
   let stringed = joined + "";
-  console.log("stringed: ", stringed);
   return (result = stringed.split("").map((item) => item * 1));
 };
 
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([4, 3, 2, 1]));
+console.log(plusOne([9]));
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+
+// Version #2
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  let arrLength = digits.length;
+  for (let i = arrLength - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] = digits[i] + 1;
+      return digits;
+    } else {
+      digits[i] = 0;
+      if (i === 0) {
+        digits.unshift(1);
+        return digits;
+      }
+    }
+  }
+};
+
+console.log(plusOne([9, 9, 9]));
 console.log(plusOne([1, 2, 3]));
 console.log(plusOne([4, 3, 2, 1]));
 console.log(plusOne([9]));
